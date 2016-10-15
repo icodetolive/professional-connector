e6cc59ac4c02ca437285ea074858e26a80b24a5d
Sugandha Naolekar <sugandha.n87@gmail.com>
2016-10-14 23:40:00 -0700

    feat: Add IBOutlet objects and connect them to the components in storyboard

--- ProfessionalConnector/LandingPageController.swift
+++ ProfessionalConnector/LandingPageController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class LandingPageViewController: UIViewController {
 
+    @IBOutlet weak var btnLinkedInSignIn: UIButton!
+    
+    @IBOutlet weak var btnViewContacts: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
