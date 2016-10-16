65fdd37506bf3741c288272405097774317e5e3d
Sugandha Naolekar <sugandha.n87@gmail.com>
2016-10-15 20:36:42 -0700

    feat: Add LinkedinSignInViewController

---
+++ ProfessionalConnector/LinkedinSignInViewController.swift
@@ -0,0 +1,10 @@
+//
+//  LinkedinSignInViewController.swift
+//  ProfessionalConnector
+//
+//  Created by Sugandha Naolekar on 10/15/16.
+//  Copyright © 2016 icode. All rights reserved.
+//
+
+import UIKit
+
